c9300f22a856320e85423eb1aa614f1358d3b77a
iirrrrffan <irfanali376246@gmail.com>
2023-12-05 11:48:26 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,40 @@
+// ----------callback-----------
+
+// function myfun(a,b,call) {
+//     call(a+b)
+// }
+// function disply(sum){
+//     console.log(sum);
+// }
+// myfun(10,20,disply)
+
+
+//-----------promise---------
+
+// let mywork=new Promise((resolve,reject)=>{
+//     let success=true
+//     if(success){
+//         resolve ("its working")
+//     }else{
+//         reject("somthing problem")
+//     }
+// })
+
+// mywork.then((msg)=>{
+//     console.log(msg);
+// })
+// .catch((msg)=>{
+//     console.error(msg); 
+// })
+
+ // ---------shift--------
+// let a=["mehaboobaa","me","there","mehabooba"];
+// a.shift();
+// console.log(a);
+
+
+
+
+
+
+
