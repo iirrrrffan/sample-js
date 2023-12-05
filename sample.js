dba0b61cc5e4e20b85304aba1a344a6b77d7350f
iirrrrffan <irfanali376246@gmail.com>
2023-12-05 13:07:59 +0530

    new

--- sample.js
+++ sample.js
@@ -32,7 +32,20 @@
 // a.shift();
 // console.log(a);
 
-
+let mywork=new Promise((resolve,reject)=>{
+    let success=false
+    if(success){
+        resolve ("its working")
+    }else{
+        reject('somthing problem')
+    }
+})
+mywork.then((msg)=>{
+   console.log(msg)
+})
+.catch((msg)=>{
+    console.error(msg)
+})
 
 
 
