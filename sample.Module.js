a981d79827ec7173b23a897976ac168b1201e119
iirrrrffan <irfanali376246@gmail.com>
2023-12-29 15:48:03 +0530

    node. server handling

--- sample.Module.js
+++ sample.Module.js
@@ -1,16 +1,12 @@
 
 // ------------server-----------
-// const http=require("http")
-// http.createServer((req,res)=>{
-//     res.write("its working")
-//     res.end()
-// }).listen(7707)
+const http=require("http")
+http.createServer((req,res)=>{
+    res.write("its working")
+    res.end()
+}).listen(7707)
 
 
-// ------fs module------
-const fs=require("fs")
-fs.writeFile("newFile.txt","done",(error)=>{
-    if(error)throw error
-})
+
 
 
