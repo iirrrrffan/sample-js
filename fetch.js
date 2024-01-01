fd5a61fa56d4c71d52c153ffa87ba7603ee20fb5
iirrrrffan <irfanali376246@gmail.com>
2024-01-01 15:33:56 +0530

    promise

---
+++ fetch.js
@@ -0,0 +1,6 @@
+// ------normal fetch-----
+// fetch(url)
+// .then(response=>response.json())
+// .then(data=>console.log(data))
+// .catch(error=>console.error("its error",error))
+
---
+++ promise.js
@@ -0,0 +1,19 @@
+// -------sample promise-----------
+// const mywork=new Promise((resolve,reject)=>{
+//     let success=true;
+//     if(success){
+//         resolve("its working") 
+//     }else{
+//         reject("somthing problem")
+//     }
+// })
+// mywork.then((msg)=>{
+//     console.log(msg);
+// })
+// .catch((msg)=>{
+//     console.error(msg);
+// })
+
+
+
+
