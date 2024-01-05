2376132cf35a527e1d4e0957a4a8797c34c6ac22
iirrrrffan <irfanali376246@gmail.com>
2024-01-05 10:39:39 +0530

    methods

---
+++ array.js
@@ -0,0 +1,39 @@
+// -------array methods-------
+
+// const ar=[10,20,30,40,50];
+// const Map=ar.map(x=>x*2)
+// console.log(Map);
+
+// const fl=[10,20,30,40,50];
+// const filt=fl.filter(x=>x%2==0)
+// console.log(filt);
+
+// const redu=[10,20,30,40,50];
+// const res=redu.reduce((x,y)=>x+y)
+// console.log(res);
+
+// const arr=[10,20,30,40,50,60];
+// arr.push(8000)
+// console.log(arr);
+
+// const arr=[10,20,30,40,50,60];
+// arr.pop()
+// console.log(arr);
+
+// const arr=[10,20,30,40,50,60];
+// arr.shift()
+// console.log(arr);
+
+// const arr=[10,20,30,40,50,60];
+// arr.unshift(400)
+// console.log(arr);
+
+
+// ---------de structuring-------
+
+// const arr=[10,20,30,40,50,60,70];
+// const [a,b,,c,...spread]=arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(spread);
---
+++ obj.js
@@ -0,0 +1,6 @@
+// -----de structuring---------
+
+// const obj={name:"irfan",age:21,salary:3000000}
+// const {name,...spread}=obj
+// console.log(name);
+// console.log(spread);
