935f117c0d6d8dec32bc349d2f5d71c3ed506159
iirrrrffan <irfanali376246@gmail.com>
2024-02-23 15:07:12 +0530

    callback

---
+++ callback.js
@@ -0,0 +1,10 @@
+// callbackfunction-----------------
+
+// const hy = ((name,call)=>{
+//     console.log("helo"+name+"!");
+//     call()
+// })
+// const a = ()=>{
+//     console.log("goodbye");
+// }
+// hy("irfan",a)
